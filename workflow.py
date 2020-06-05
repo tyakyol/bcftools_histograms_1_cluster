c481b3977259cbaf78eee8f17f1cd55de03a7f0f
Turgut Yigit Akyol tya@mbg.au.dk <tyakyol@fe1.genomedk.net>
2020-06-05 17:00:42 +0200

    first version of the workflow.

---
+++ workflow.py
@@ -0,0 +1,28 @@
+from gwf import Workflow
+from templates import *
+
+gwf = Workflow()
+
+vcf_file = '../../InRoot/variant_calling_7/results/20200531_raw_variants.vcf'
+
+gwf.target_from_template('selectIndel',
+                         select_indel(vcf=vcf_file,
+                                      indel='data/only_indels.vcf',
+                                      idx='data/only_indels.vcf.idx'
+                                      ))
+
+gwf.target_from_template('selectSNPs',
+                         select_snp(vcf=vcf_file,
+                                    snp='data/only_snps.vcf',
+                                    idx='data/only_snps.vcf.idx'
+                                    ))
+
+gwf.target_from_template('indeltotsv',
+                         snp_to_tsv(vcf='data/only_indels.vcf',
+                                    tsv='data/only_indels.tsv'
+                                    ))
+
+gwf.target_from_template('SNPtotsv',
+                         snp_to_tsv(vcf='data/only_snps.vcf',
+                                    tsv='data/only_snps.tsv'
+                                    ))
