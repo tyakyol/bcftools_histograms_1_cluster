68fd6ee433fee081a6a555ea19ab1f2a34c3ae37
Turgut Yigit Akyol tya@mbg.au.dk <tyakyol@fe1.genomedk.net>
2020-06-05 17:02:05 +0200

    first version of the R script of SNP hist.

---
+++ snp_histograms.R
@@ -0,0 +1,41 @@
+library(dplyr)
+
+snps = read.delim('data/only_snps.tsv',
+                  header = TRUE,
+                  sep = '\t')
+not_any_na = function(x) all(!is.na(x))
+snps = select_if(snps, not_any_na)
+snps = snps[, -16]
+snps[, 14] = as.numeric(snps[, 14])
+snps = snps[snps$X.18.results.sorted_Gifu_R.bam.GT != './.', ]
+ref = snps[snps$X.18.results.sorted_Gifu_R.bam.GT =='0/0', ]
+het = snps[snps$X.18.results.sorted_Gifu_R.bam.GT =='0/1', ]
+alt = snps[snps$X.18.results.sorted_Gifu_R.bam.GT =='1/1', ]
+
+pdf('results/all_SNPs_hist.pdf')
+for(i in c(3, 4, 6:16)) {
+  hist(x = as.numeric(snps[, i]), col = 'gold', breaks = 300,
+       main = colnames(snps)[i])
+}
+dev.off()
+
+pdf('results/ref_SNPs_hist.pdf')
+for(i in c(3, 4, 6:16)) {
+  hist(x = as.numeric(ref[, i]), col = 'gold', breaks = 300,
+       main = colnames(ref)[i])
+}
+dev.off()
+
+pdf('results/het_SNPs_hist.pdf')
+for(i in c(3, 4, 6:16)) {
+  hist(x = as.numeric(het[, i]), col = 'gold', breaks = 300,
+       main = colnames(het)[i])
+}
+dev.off()
+
+pdf('results/alt_SNPs_hist.pdf')
+for(i in c(3, 4, 6:16)) {
+  hist(x = as.numeric(alt[, i]), col = 'gold', breaks = 300,
+       main = colnames(alt)[i])
+}
+dev.off()
